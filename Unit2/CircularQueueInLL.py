7c8a8bae794249e1f481047fa4ab0bd8b1f0011d
KIRUTHIK M R <kiruthikpurpose@gmail.com>
2024-11-13 06:43:19 +0530

    Made stack and queue programs prettier

--- Unit2/CircularQueueInLL.py
+++ Unit2/CircularQueueInLL.py
@@ -7,6 +7,7 @@ class CircularQueueLinkedList:
     def __init__(self):
         self.tail = None
         self.size = 0
+
     def enqueue(self, item):
         new_node = Node(item)
         if self.is_empty():
@@ -17,6 +18,7 @@ class CircularQueueLinkedList:
             self.tail.next = new_node
             self.tail = new_node
         self.size += 1
+
     def dequeue(self):
         if self.is_empty():
             return None
@@ -27,9 +29,12 @@ class CircularQueueLinkedList:
             self.tail.next = head.next
         self.size -= 1
         return head.data
+    
     def peek_front(self):
         return self.tail.next.data if not self.is_empty() else None
+    
     def is_empty(self):
         return self.tail is None
+    
     def current_size(self):
         return self.size
